97b309bcd5cafcff75d11c8f9287bf82e5b16bbe
Sean Maguire <smaguire@nanigans.com>
2021-04-15 19:54:42 -0400

    fixes to db and make_users

--- slay/__init__.py
+++ slay/__init__.py
@@ -9,10 +9,15 @@ from flask_marshmallow import Marshmallow
 
 from .reminder import Reminder
 
-db_file = os.path.join(os.getcwd(), 'slay.db')
+
 template_dir = os.path.abspath(os.path.join(os.getcwd(), 'www'))
 app = Flask(__name__, template_folder=template_dir)
-app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_file}'
+
+db_file = os.path.join(os.getcwd(), 'slay.db')
+db_uri = os.environ.get('DATABASE_URL', f'sqlite:///{db_file}')
+db_uri = db_uri.replace('postgres://', 'postgresql://')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
@@ -21,8 +26,3 @@ Reminder.run()
 with app.app_context():
     from . import routes
     db.create_all()
-
-
-
-
-
